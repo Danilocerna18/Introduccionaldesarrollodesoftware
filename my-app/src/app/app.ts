957c50e4cdd350246478514102b756119f19f7a5
Danil38 <danilo.cerna@ufm.edu>
2026-02-25 03:55:41 -0600

    corrections
    this is a little better but it still soes not work perfecly

--- my-app/src/app/app.ts
+++ my-app/src/app/app.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+/*import { Component } from '@angular/core';
 
 import { HeaderComponent } from '../components/header/header';
 import { PerfilComponent } from '../components/perfil/perfil';
@@ -22,5 +22,16 @@ import { ContactoComponent } from '../components/contacto/contacto';
   ],
   templateUrl: './app.html',
   styleUrl: './app.css'
+})
+export class App {}
+*/
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterModule],  // 👈 ESTO ES LO QUE FALTABA
+  templateUrl: './app.html'
 })
 export class App {}
