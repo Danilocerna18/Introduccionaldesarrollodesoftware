9c9ad82398e3dfd05b16326b120d011f764e0cd6
Danil38 <danilo.cerna@ufm.edu>
2026-03-13 20:40:39 -0600

    i create my service with no real data but its just for do it

--- my-finalcv/src/components/cursos/cursos.ts
+++ my-finalcv/src/components/cursos/cursos.ts
@@ -1,11 +1,20 @@
 import { Component } from '@angular/core';
+import { NgFor } from '@angular/common';
+import { DataService } from '../../app/data';
 
 @Component({
   selector: 'app-cursos',
-  imports: [],
+  standalone: true,
+  imports: [NgFor],
   templateUrl: './cursos.html',
-  styleUrl: './cursos.css',
+  styleUrl: './cursos.css'
 })
 export class Cursos {
+
+  jobs: string[] = [];
+
+  constructor(private dataService: DataService) {
+    this.jobs = this.dataService.getJobs();
+  }
 
 }
--- my-finalcv/src/components/skills/skills.ts
+++ my-finalcv/src/components/skills/skills.ts
@@ -1,11 +1,20 @@
 import { Component } from '@angular/core';
+import { NgFor, UpperCasePipe } from '@angular/common';
+import { DataService } from '../../app/data';
 
 @Component({
   selector: 'app-skills',
-  imports: [],
+  standalone: true,
+  imports: [NgFor, UpperCasePipe],
   templateUrl: './skills.html',
-  styleUrl: './skills.css',
+  styleUrl: './skills.css'
 })
 export class Skills {
+
+  skills: string[] = [];
+
+  constructor(private dataService: DataService) {
+    this.skills = this.dataService.getSkills();
+  }
 
 }
