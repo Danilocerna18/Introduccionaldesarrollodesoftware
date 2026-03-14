5820fb78cf65beec4f57921a46f136f77a301a91
Danil38 <danilo.cerna@ufm.edu>
2026-03-13 19:30:58 -0600

    this is the commit with my routes

--- my-finalcv/src/app/app.routes.ts
+++ my-finalcv/src/app/app.routes.ts
@@ -1,3 +1,35 @@
-import { Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { Header } from '../components/header/header';
+import { Aboutme } from '../components/aboutme/aboutme';
+import { Contact } from '../components/contact/contact';
+import { Cursos } from '../components/cursos/cursos';
+import { Educacion } from '../components/educacion/educacion';
+import { Experiencia } from '../components/experiencia/experiencia';
+import { Home } from '../components/home/home';
+import { Skills } from '../components/skills/skills';
+
+export const routes: Routes = [
+
+  { path: 'skills', component: Skills },
+  { path: 'aboutme', component: Aboutme },
+
+  {
+    path: 'experiencia',
+    component: Experiencia,
+    children: [
+      { path: 'cursos', component: Cursos },
+      { path: 'educacion', component: Educacion }
+    ]
+  },
+
+  { path: '', redirectTo: '/aboutme', pathMatch: 'full' }
+
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
